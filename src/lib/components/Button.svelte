bd140ef7fa37b3a47a23607f8542c7d52d5171fc
antonio-pas <antonio.b.passos18@gmail.com>
2023-08-06 10:39:11 -0300

    Added buttons

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let type: 'primary' | 'secondary' | 'warning' = 'primary'
+</script>
+
+
+<button class="px-4 py-2 font-bold transition rounded-lg active:scale-95 
+  {type == 'primary' ? 
+    'bg-purple-500 text-white hover:bg-purple-700' : 
+    type == 'secondary' ?
+      'bg-transparent text-slate-800 border-slate-300 border hover:bg-slate-200' :
+      'bg-red-700 text-white hover:bg-red-800'} ">Hello</button>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      'sans': ["Inter", "system-ui", 'sans-serif']
+    },
+    extend: {}
+  },
+  plugins: []
+}
